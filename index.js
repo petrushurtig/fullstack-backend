b9f6920bff9108bf1c7b11847fbfb93a2ec8c627
Petrus Hurtig <petrushurtig@hotmail.com>
2022-04-04 12:57:47 +0300

    uibuild

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post('/api/persons', (req, res, next) => {
 })
 app.delete('/api/persons/:id', (req, res, next) => {
     Person.findByIdAndRemove(req.params.id)
-        .then(res => {
+        .then(() => {
             res.status(204).end()
         })
         .catch(error => next(error))
